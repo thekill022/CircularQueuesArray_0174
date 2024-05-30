b72367a8807662ff4efa7aaa3d50f064396cc7e0
thekill022 <gibfathoni@gmail.com>
2024-05-30 10:29:19 +0700

    set value front dan rear = -1

--- CircularQueuesArray_0174/CircularQueuesArray_0174.cpp
+++ CircularQueuesArray_0174/CircularQueuesArray_0174.cpp
@@ -6,6 +6,13 @@ class Queues {
     //deklarasi variabel front, rear, dan max, dengan nilai max = 5
     int FRONT, REAR, max = 5;
 
+public :
+    //set value front dan rear = -1
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
 };
 
 int main()
